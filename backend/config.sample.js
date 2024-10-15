cd5a14edb697170954cdb148b857f294e57998c8
garrikyx <garrisonkoh12315@gmail.com>
2024-10-15 22:45:46 +0800

    initialise firebase

---
+++ backend/config.sample.js
@@ -0,0 +1,10 @@
+// Create config.js and replace with the actual config
+export const firebaseConfig = {
+    apiKey: "your-firebase-api-key-here",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id",
+  };
+  
